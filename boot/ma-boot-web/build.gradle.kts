f075f91b9ec2453771e6f898915e6ba86a2b5e0e
holeman79 <holeman79@gmail.com>
2025-07-14 22:51:55 +0900

    asciidoctor 설정

--- boot/ma-boot-web/build.gradle.kts
+++ boot/ma-boot-web/build.gradle.kts
@@ -1,8 +1,10 @@
+
+import org.asciidoctor.gradle.jvm.AsciidoctorTask
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
 	id("org.asciidoctor.jvm.convert") version "3.3.2"
 }
-
-val snippetsDir by extra { file("build/generated-snippets") }
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
@@ -21,27 +23,75 @@ dependencies {
 	runtimeOnly(project(":support:ma-crypto-core"))
 	runtimeOnly(project(":storage:ma-db-core"))
 	runtimeOnly(project(":storage:ma-redis-core"))
+
+	// AsciiDocs
+	val asciidoctorExt: Configuration by configurations.creating
+	asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
 }
 
+configure<org.springframework.boot.gradle.dsl.SpringBootExtension> {
+	buildInfo()
+}
+
+
 tasks {
-	test {
+	// Spring REST Docs
+	val snippetsDir = file("build/generated-snippets")
+
+	withType<Test> {
 		useJUnitPlatform()
 		outputs.dir(snippetsDir)
-		testLogging {
-			events("passed", "skipped", "failed")
+	}
+
+	// AsciiDocs
+	withType<AsciidoctorTask> {
+		asciidoctor.get().doFirst {
+			delete {
+				file("build/docs/asciidoc")
+				file("src/main/resources/static/docs")
+			}
+		}
+		configurations("asciidoctorExt")
+		inputs.dir(snippetsDir)
+		dependsOn(test)
+		// 빌드 시, 아래 경로의 파일 삭제
+		forkOptions {
+			jvmArgs(
+				"--add-opens", "java.base/sun.nio.ch=ALL-UNNAMED",
+				"--add-opens", "java.base/java.io=ALL-UNNAMED"
+			)
 		}
 	}
 
-	asciidoctor {
-		inputs.dir(snippetsDir)
-		dependsOn(test)
+	withType<BootJar> {
+		delete {
+			file("build/docs/asciidoc")
+			file("src/main/resources/static/docs")
+		}
+		dependsOn(":domain:ma-domain-core:test")
+		dependsOn(asciidoctor)
+		from("${asciidoctor.get().outputDir}/html5") {
+			into("static/docs")
+		}
+
+		// BootJar 파일명
+		archiveBaseName.set("api")
+		// BootJar 버전
+		archiveVersion.set("")
+	}
+
+	val copyDocument by registering(Copy::class) {
+		dependsOn(asciidoctor)
+
+		from(file("build/docs/asciidoc/"))
+		into(file("src/main/resources/static/docs"))
+	}
+
+	build {
+		dependsOn(copyDocument)
 	}
 }
 
-// 빌드 시 asciidoctor 태스크가 실행되도록 설정
-tasks.bootJar {
-	dependsOn(tasks.asciidoctor)
-	from("${tasks.asciidoctor.get().outputDir}") {
-		into("static/docs")
-	}
+tasks.jar {
+	enabled = false
 }
