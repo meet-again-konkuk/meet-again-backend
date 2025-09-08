8820aaecfddb7691e53a8938edd2e4a6b830e8ef
holeman79 <holeman79@gmail.com>
2025-09-08 22:31:39 +0900

    feat(Jwt, Exception) : AuthTokenExceptions 추가

---
+++ domain/ma-domain-core/src/main/kotlin/com/konkuk/ma/exception/AuthTokenExceptions.kt
@@ -0,0 +1,36 @@
+package com.konkuk.ma.exception
+
+import kotlin.reflect.KFunction
+
+class AuthTokenException(
+    token: String,
+
+    val jwtExceptionType: JwtExceptionType,
+
+    throwable: Throwable? = null,
+
+    logLevel: LogLevel = LogLevel.INFO,
+
+    callerFunction: KFunction<*>
+) : BusinessException(
+    message = "${jwtExceptionType.message} token : $token",
+    callerFunction = callerFunction,
+    cause = throwable,
+    logLevel = logLevel
+) {
+    fun isExpired(): Boolean {
+        return jwtExceptionType.isExpired()
+    }
+
+    fun isMalformed(): Boolean {
+        return jwtExceptionType.isMalformed()
+    }
+
+    fun isInvalid(): Boolean {
+        return jwtExceptionType.isInvalid()
+    }
+
+    fun isOtherError(): Boolean {
+        return jwtExceptionType.isOtherError()
+    }
+}
