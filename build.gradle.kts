954e3d62497f4f2dc69cb4cfce3a5a3cd70ddb11
holeman79 <holeman79@gmail.com>
2025-09-12 23:57:44 +0900

    fix(config-logging) : circular dependency 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,10 @@ subprojects {
 	}
 
 	dependencies {
-		implementation(project(":config:ma-config-logging"))
+//		implementation(project(":config:ma-config-logging"))
+		if (project.path != ":config:ma-config-logging") {
+			implementation(project(":config:ma-config-logging"))
+		}
 
 		// KoTest
 		testImplementation("io.kotest:kotest-runner-junit5-jvm:5.5.5")
--- domain/ma-domain-core/src/main/kotlin/com/konkuk/ma/domain/auth/domain/SmsVerification.kt
+++ domain/ma-domain-core/src/main/kotlin/com/konkuk/ma/domain/auth/domain/SmsVerification.kt
@@ -3,8 +3,4 @@ package com.konkuk.ma.domain.auth.domain
 class SmsVerification(
     val phoneNumber: String,
     val verificationCode: Int
-) {
-    fun isVerified(generatedVerificationCode: Int?): Boolean {
-        return verificationCode == generatedVerificationCode
-    }
-}
+)
